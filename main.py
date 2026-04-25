aa5cecd5c76d16d30774d52cc8769bca17117381
Santiago Guzman <santm099@gmail.com>
2026-04-25 11:25:58 -0500

    fix main py

--- main.py
+++ main.py
@@ -9,9 +9,29 @@ from operations_db import (create_movie_db,
 
 app = FastAPI(lifespan=create_all_tables)
 
+
 @app.get("/")
 async def root():
-    return {"message": "Welcome to Movies API"}
+    return {"message": "Welcome to Movies API 🎬"}
+
+
+@app.post("/movies", response_model=Movie)
+async def create_movie(movie: MovieBase, session: SessionDep):
+    return create_movie_db(movie, session)
+
+
+@app.get("/movies", response_model=list[Movie])
+async def show_movies(session: SessionDep):
+    return show_all_movies_db(session)
+
+
+@app.get("/movies/{id}", response_model=Movie)
+async def show_one_movie(id: int, session: SessionDep):
+    movie = find_one_movie_db(id, session)
+    if not movie:
+        raise HTTPException(status_code=404, detail=f"Movie {id} not found")
+    return movie
+
 
 @app.patch("/movies/{id}", response_model=Movie)
 async def update_movie(id: int, movie: MovieUpdate, session: SessionDep):
@@ -19,6 +39,7 @@ async def update_movie(id: int, movie: MovieUpdate, session: SessionDep):
     if not updated:
         raise HTTPException(status_code=404, detail=f"Movie {id} not found")
     return updated
+
 
 @app.delete("/movies/{id}", response_model=Movie)
 async def delete_movie(id: int, session: SessionDep):
