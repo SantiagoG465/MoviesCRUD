a5b5d181ea9fe944c022e9730c47217a784bd2bd
Santiago Guzman <santm099@gmail.com>
2026-04-25 11:44:54 -0500

    reviews movies

--- models.py
+++ models.py
@@ -22,3 +22,21 @@ class MovieUpdate(SQLModel):
     genre: Optional[str] = None
     rating: Optional[float] = None
     watched: Optional[bool] = None
+
+class ReviewBase(SQLModel):
+    comment: str
+    score: float
+    date: str
+    is_active: bool = True
+    movie_id: int = Field(foreign_key="movie.id")
+
+
+class Review(ReviewBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class ReviewUpdate(SQLModel):
+    comment: Optional[str] = None
+    score: Optional[float] = None
+    date: Optional[str] = None
+    is_active: Optional[bool] = None
